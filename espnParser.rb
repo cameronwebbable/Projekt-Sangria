0cdbc1f287665b2a156960863947e14789ffced9
webbca01 <cwebb02@gmail.com>
2010-11-03 13:54:51 -0500

    Adding playerID. Woots.
    Also adding in link for actual scores.
    
    Signed-off-by: webbca01 <cwebb02@gmail.com>

--- espnParser.rb
+++ espnParser.rb
@@ -17,7 +17,8 @@ require 'nokogiri'
 #Ok, so we need to get the web page and then get our elements. Shibby!
 for i in (1..30)
     i = i * 40
-	url = "http://games.espn.go.com/ffl/tools/projections?&scoringPeriodId=1&seasonId=2010&leagueId=521760&startIndex="+ i.to_s
+	url = "http://games.espn.go.com/ffl/tools/projections?&scoringPeriodId=1&seasonId=2010&startIndex="+ i.to_s
+	#url = "http://games.espn.go.com/ffl/leaders?scoringPeriodId=1"
 	puts url
 	doc = Nokogiri::HTML(open(url))
 	players = doc.xpath('//tr/td[@class="playertablePlayerName"]')
@@ -31,15 +32,14 @@ for i in (1..30)
 
 	  playerIndex = players.index(node)
 	  playerRegEx = Regexp.new '[\s*|,|/\302\240/]', nil, 'n'
-
+	  playerID = node.xpath('.//a').attribute('playerid')
+	  puts playerID
 	  if RUBY_VERSION.delete('.').to_i > 190
 		  #we enforce ASCII-8Bit to make this happy for ruby 1.9...
 		  player = node.text.force_encoding("ASCII-8Bit").split(playerRegEx).delete_if {|x| x == ""}
 	  else
 		  player = node.text.split(playerRegEx).delete_if {|x| x == ""}
 	  end
-		  
-	  puts(player)
 	  #puts(projection[players.index(node)].text)
 	  #puts(stats[playerIndex*10].text + " " + stats[playerIndex*10+1].text) 
 	end
