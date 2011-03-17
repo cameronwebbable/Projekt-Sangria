5dac876a8a2395dc9ff59bf03db2ee7b6c81fe6b
webbca01 <cwebb02@gmail.com>
2011-03-17 18:16:51 -0500

    Added rails project

---
+++ ProjectSangria/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectSangria::Application.config.secret_token = '4928039e050a1af4599132bfc75a77bc0144384966ac1f190554ae9c86fc7d82802362a973338d43f3e22cfde2b9ba71dc49ac1f464f920d1cfc1e6f11328b00'
